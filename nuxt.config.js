9eb5b5ac11a3b04d02243429c33fb291a31dc108
bikailai <bgx1012@163.com>
2025-08-21 11:43:38 +0800

    2025年8月21日11:43:36

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    // '@nuxt/eslint',
+    '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui-pro',
     '@nuxt/content',
